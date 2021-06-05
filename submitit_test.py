c6d0ca4818b715f87929bd1737d91a0161413b58
Innovator_Arjun <vsarjun412@gmail.com>
2021-06-05 07:47:17 -0400

    Push

--- submitit_test.py
+++ submitit_test.py
@@ -1,28 +1,37 @@
 import submitit
+import wandb
 
-def add(a, b):
-    return a + b
+def add(a):
 
-# executor is the submission interface (logs are dumped in the folder)
-executor = submitit.AutoExecutor(folder="log_test")
+    wandb.login()
+# 1. Start a W&B run
+    wandb.init(project='gpt6')
 
-# set timeout in min, and partition for running the job
+# 2. Save model inputs and hyperparameters
+    config = wandb.config
+    config.learning_rate = 0.01
+
+# Model training code here ...
+
+# 3. Log metrics over time to visualize performance
+    for i in range (a):
+        print(i)
+        wandb.log({"loss": i})
+
+    # executor is the submission interface (logs are dumped in the folder)
+executor = submitit.AutoExecutor(folder="logs")
+
+    # set timeout in min, and partition for running the job
 num_gpus = 1
-workers_per_gpu = 10
 
 executor.update_parameters(
-        timeout_min=1:0:0,
-        gpus_per_node=num_gpus,
-        slurm_additional_parameters={"account": "rrg-bengioy-ad"},
-        tasks_per_node=num_gpus,
-        cpus_per_task=workers_per_gpu,
-        slurm_mem="16G"#16G
-        
-    )
+                            timeout_min=15,
+                            gpus_per_node=num_gpus,
+                            slurm_additional_parameters={"account": "def-bengioy"},
+                             tasks_per_node=num_gpus,
+                             slurm_mem="2G"#16G
 
-job = executor.submit(add, 5, 7)  # will compute add(5, 7)
+                              )
+job = executor.submit(add, 5)  # will compute add(5, 7)
 print(job.job_id)  # ID of your job
-
-output = job.result()  # waits for completion and returns output
-assert output == 12  # 5 + 7 = 12...  your addition was computed in the cluster
-print('Job Submitted')
+print('Job Sucess')
